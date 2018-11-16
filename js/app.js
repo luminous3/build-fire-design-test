febd46b18c3d9c5cd51a07c2f3a607c07697f60c
Alexandra Mirtcheva <alex@solanalabs.io>
2018-11-15 17:44:58 -0800

    use moment for dates

--- js/app.js
+++ js/app.js
@@ -1,34 +1,33 @@
+function loadTasks(taskList) {
+  let container = document.getElementById('taskContainer')
+  container.innerHTML = ''
 
-function loadTasks(taskList){
-    let container = document.getElementById('taskContainer');
-    container.innerHTML='';
-
-    taskList.forEach((task,i)=>{
-        let divTask = document.createElement('div');
-        divTask.className="taskCard";
-        divTask.innerHTML = `
-            ${task.img?"<img src='" + task.img + "' />":""}
-            <h4> <input type="checkbox" ${task.completed?"checked":""} title="check completed">  ${task.title + ' ' + i}</h4>
-            <span>created on ${task.createdOn} by ${task.createdBy}</span>
+  taskList.forEach((task, i) => {
+    var now = moment()
+    let divTask = document.createElement('ul')
+    divTask.className = 'taskCard'
+    divTask.innerHTML = `
+            <li>
+            ${task.img ? "<img src='" + task.img + "' />" : ''}
+            <h4> <input type="checkbox" ${task.completed ? 'checked' : ''} title="check completed">  ${task.title +
+      ' ' +
+      i}</h4>
+            <span>created on ${now.format('MMMM Do YYYY, h:mm:ss a')} by ${task.createdBy}</span>
             <p>${task.description}</p>
-            <span>Due on ${task.dueDate}</span>            
-        `;
+            <span>Due on ${task.dueDate}</span>
+            </li>
+        `
 
-        if(task.completed)
-            divTask.classList.add("taskCompleted");
-        else if(task.dueDate < Date.now() )
-            divTask.classList.add("taskLate");
+    if (task.completed) divTask.classList.add('taskCompleted')
+    else if (task.dueDate < Date.now()) divTask.classList.add('taskLate')
 
-
-        container.appendChild(divTask);
-    });
+    container.appendChild(divTask)
+  })
 }
 
+loadTasks(taskList)
 
-loadTasks(taskList);
-
-
-function addTask(task){
-    taskList.unshift(task);
-    loadTasks(taskList);
+function addTask(task) {
+  taskList.unshift(task)
+  loadTasks(taskList)
 }
