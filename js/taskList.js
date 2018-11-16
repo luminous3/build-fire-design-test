89e80522a1d4a89e7195ff793b2e818c7c09dd37
Alexandra Mirtcheva <alex@solanalabs.io>
2018-11-16 11:03:28 -0800

    move completed tasks to bottom

--- js/taskList.js
+++ js/taskList.js
@@ -1,25 +1,30 @@
 let taskList = [
   {
-    title: 'Submit invoices',
-    description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.", /// <= Pretty long
-    createdBy: 'Jane Smith',
-    dueDate: '01/01/2020', /// Has enough time to finish
-  },
-  {
     title: 'Call Jamie',
     description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.",
     createdBy: 'Jane Smith',
     dueDate: '01/01/2018', /// <== Past due.
   },
   {
-    title: 'Organize designs',
+    title: 'Order new phone',
     description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.",
     createdBy: 'Jane Smith',
-    dueDate: '01/01/2020',
-    completed: true,
+    dueDate: '01/01/2018',
+  },
+  {
+    title: 'Submit invoices',
+    description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.", /// <= Pretty long
+    createdBy: 'Jane Smith',
+    dueDate: '01/01/2020', /// Has enough time to finish
   },
   {
     title: 'Find catering for lunch event',
+    description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.",
+    createdBy: 'Jane Smith',
+    dueDate: '01/01/2020',
+  },
+  {
+    title: 'Complete todo list',
     description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.",
     createdBy: 'Jane Smith',
     dueDate: '01/01/2020',
@@ -32,15 +37,10 @@ let taskList = [
     completed: true,
   },
   {
-    title: 'Order new phone',
-    description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.",
-    createdBy: 'Jane Smith',
-    dueDate: '01/01/2018',
-  },
-  {
-    title: 'Complete todo list',
+    title: 'Organize designs',
     description: "This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back.",
     createdBy: 'Jane Smith',
     dueDate: '01/01/2020',
+    completed: true,
   },
 ]
